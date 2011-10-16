934506e35fdc41662c410128d7a3d0d55fb6b6eb
Túbal <tubal@MacBook-Pro-de-Tubal.local>
2011-10-16 19:22:01 +0200

    +Added  type="text/javascript" attribute for (X)HTML 4 compatibility
    +Changed target node for script insertion following Google's suggestion:
       - document.getElementsByTagName("head")[0] is not the most compatible way of selecting a target node for script tags insertion since some sites do not even have a "head" section.
       - There are 2 alternatives for cross-site and cross-browser compatibility:
          1) var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement // (taken from jQuery 1.6.4)
          2) Use Google's technique:
              var scriptNode =  document.getElementsByTagName("script")[0];
              scriptNode.parentNode.insertBefore(node, scriptNode);
        - I've implemented Google's approach.

--- dist/script.js
+++ dist/script.js
@@ -10,7 +10,7 @@
   else this[name] = definition()
 }('$script', function() {
   var win = this, doc = document
-    , head = doc.getElementsByTagName('head')[0]
+    , script = 'script', scriptNode = doc.getElementsByTagName(script)[0]
     , validBase = /^https?:\/\//
     , old = win.$script, list = {}, ids = {}, delay = {}, scriptpath
     , scripts = {}, s = 'string', f = false
@@ -68,18 +68,20 @@
   }
 
   function create(path, fn) {
-    var el = doc.createElement('script')
+    var el = doc.createElement(script)
       , loaded = f
     el.onload = el.onerror = el[onreadystatechange] = function () {
       if ((el[readyState] && !(/^c|loade/.test(el[readyState]))) || loaded) return;
       el.onload = el[onreadystatechange] = null
+      el = undefined // Dereference the script
       loaded = 1
       scripts[path] = 2
       fn()
     }
+    el.type = "text/javascript"
     el.async = 1
     el.src = path
-    head.insertBefore(el, head.firstChild)
+    scriptNode.parentNode.insertBefore(el, scriptNode)
   }
 
   $script.get = create
@@ -95,6 +97,7 @@
   $script.path = function(p) {
     scriptpath = p
   }
+
   $script.ready = function(deps, ready, req) {
     deps = deps[push] ? deps : [deps]
     var missing = [];
@@ -115,4 +118,4 @@
   }
 
   return $script
-})
+});
