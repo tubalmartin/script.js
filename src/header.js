30d4dd892120b2cf52d2fac3bd4ff7ffd1996f7c
Túbal Martín <tubalmartin@gmail.com>
2011-10-18 00:49:29 +0300

    Update src/header.js

--- src/header.js
+++ src/header.js
@@ -3,4 +3,6 @@
   * https://github.com/ded/script.js
   * (c) Dustin Diaz, Jacob Thornton 2011
   * License: MIT
+  * Forked version by Tubal Martin
+  * https://github.com/tubalmartin/script.js
   */
